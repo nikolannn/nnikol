0c21ddcda0857212b5abc0a5ecbb25d33740c11a
Nicole Ann <annics12345@gmail.com>
2025-03-18 21:34:52 +0800

    first commit

--- app/Middleware/PreventBackHistory.php
+++ app/Middleware/PreventBackHistory.php
@@ -7,19 +7,12 @@ use Illuminate\Http\Request;
 
 class PreventBackHistory
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
 
-        return $response->header('Cache-Control', 'no-cache, no-store, must-revalidate')
+        return $response->header('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0')
                         ->header('Pragma', 'no-cache')
-                        ->header('Expires', '0');
+                        ->header('Expires', 'Sat, 01 Jan 2000 00:00:00 GMT');
     }
 }
